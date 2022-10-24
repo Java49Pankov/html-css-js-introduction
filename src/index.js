2d05ec38026e98d73c5d9af3486ec74ba05c3b12
EvgenyPankov <volvovnl123@gmail.com>
2022-10-24 21:32:03 +0300

    adding audio, animation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsAudio = document.querySelector(".details-audio");
+const detailsContainer = document.querySelector(".details-container");
 const mainClass = document.querySelector(".main-class");
 const HIDDEN = "hidden";
+const IS_TINY = "is-tiny";
 
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
+    detailsAudio.setAttribute("src", anchor.getAttribute("data-details-audio"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
 
@@ -15,11 +19,20 @@ for (let i = 0; i < anchors.length; i++) {
         console.log("event-click on ", anchors[i]);
         showDetails();
         setDetails(anchors[i]);
+        detailsAudio.play();
     })
 }
 
 function showDetails() {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_TINY);
+    setTimeout(function () {
+        detailsContainer.classList.remove(IS_TINY);
+    }, 1)
+    setTimeout(function () {
+        detailsAudio.pause();
+        detailsAudio.currentTime = 0;
+    }, 5000);
 }
 
 function hideDetails() {
