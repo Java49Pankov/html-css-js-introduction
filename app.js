eb02dff592664b5a46234addaf58fb5768f4c336
EvgenyPankov <volvovnl123@gmail.com>
2022-10-30 18:55:59 +0200

    Hw #11

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // console.log("Hello world");
-// for (var i = 0; i < 3; i++) {
-//     setTimeout(function () { console.log(i) }); 
-// } =>3 3 3
 
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(function () { console.log(i) });
-// } => 0 1 2
+/*
+for (var i = 0; i < 3; i++) {
+    setTimeout(function () { console.log(i) }); 
+} // =>3 3 3
 
-// for (var i = 0; i < 3; i++) {
-//     console.log(i);
-// }
-// console.log(i);
+for (let i = 0; i < 3; i++) {
+    setTimeout(function () { console.log(i) });
+} // => 0 1 2
+*/
+
+/*
+for (var i = 0; i < 3; i++) {
+    console.log(i); // => 0 1 2
+}
+console.log(i); // => 3
+*/
 
 // function sum(op1, op2) {
 //     let res = op1 + op2;
@@ -18,28 +23,22 @@
 // }
 // console.log(sum(10, 20));
 
-function sumDigits(num) {
-    let sum = 0;
-    while (num) {
-        let rem = num % 10;
-        sum += rem;
-        num = (num - rem) / 10;
-    }
-    return sum;
-}
-console.log(sumDigits(123));
 
-function digitsSum(number) {
-    if (number < 0) {
-        number = -number;
-    }
+function sumDigits(number) {
+    //compute sum of digits for the intenger numbers
+    // if (number < 0) {
+    //     number = -number;
+    //}
+    number = Math.abs(number);
+    number = Math.trunc(number); // get rid of the fractional part
     let sum = 0;
-    let rem = 0;
     do {
-        rem = number % 10;
+        let rem = number % 10;
         sum += rem;
         number = (number - rem) / 10;
+        // number = Math.floor(number / 10);
     } while (number != 0);
     return sum;
 }
-console.log(digitsSum(-641));
+console.log(sumDigits(-643.876));
+
