fc767c60e88d20cfc9d76f701e1f96c6463401f7
EvgenyPankov <volvovnl123@gmail.com>
2022-11-04 00:52:56 +0200

    check Teudat zehut and random arrays

--- HwApp.js
+++ HwApp.js
@@ -1,61 +1,71 @@
-function fromNumberToString(number, base) {
-    number = Math.abs(number);
-    base = Math.abs(base);
-    let res = '';
-    do {
-        let digit = number % base;
-        if (digit >= 10) {
-            digit = digit + 87;
-        } else if (digit < 10) {
-            digit = digit + 48;
-        }
-        let sym = String.fromCharCode(digit);
-        res = sym + res;
-        number = Math.trunc(number / base);
-    } while (number != 0);
-    return res;
-}
-console.log(fromNumberToString(900550, 36));
-console.log(fromNumberToString(-900550, 36));
-console.log(fromNumberToString('900550', 36));
-console.log(fromNumberToString(900550, -36));
-console.log(fromNumberToString(900550, '36'));
-console.log(fromNumberToString(46016237, 36));
-console.log(fromNumberToString(11483, 2));
 
-function fromStringToNumber(string, base) {
-    base = Math.abs(base);
-    let res = 0;
-    for (let i = 0; i < string.length; i++) {
-        let code = string[i].charCodeAt();
-        if (base == 2) {
-            code = string[i] == 0 ? 0 : 1;
-        } else if (code >= 10) {
-            code = code - 87;
-        } else if (code < 10) {
-            code = code - 48;
-        }
-        res = res * base + code;
+function checkTeudatZehut(teudatStrNumber) {
+    if (teudatStrNumber.length < 9 || teudatStrNumber.length > 9) {
+        return false;
     }
-    return res;
-}
-console.log(fromStringToNumber('java', 36));
-console.log(fromStringToNumber('java', -36));
-console.log(fromStringToNumber('react', 36));
-console.log(fromStringToNumber('10110011011011', 2));
 
-function fromNumberToString1(number, base) {
-    number = Math.abs(number);
-    base = Math.abs(base);
-    let res = '';
-    do {
-        let digit = number % base;
-        let sym = digit.toString(base);
-        res = sym + res;
-        number = Math.trunc(number / base);
-    } while (number != 0);
-    return res;
+    let arNum = Array.from(teudatStrNumber);
+    let arCode = [];
+
+    arCode = arNum.map(function (num, index) {
+        let digit = +num;
+        let res = 0;
+        if (index % 2 !== 0) {
+            res = digit * 2;
+            if (res > 9) {
+                res -= 9;
+            }
+        } else {
+            res = digit;
+        }
+        return res;
+    });
+    console.log(arCode);
+
+    let sum = arCode.reduce(function (previos, current) {
+        return previos + current;
+    }, 0);
+    console.log(sum);
+
+    return sum % 2 === 0;
 }
-console.log(fromNumberToString1(900550, 36));
-console.log(fromNumberToString1(46016237, 36));
-console.log(fromNumberToString1(11483, 2));
+console.log(checkTeudatZehut("376628202"));
+
+function generateRandomTeudatZehut() {
+    let randomNum = [];
+    for (let i = 0; i < 8; i++) {
+        randomNum.push(Math.round(Math.random() * 9));
+    }
+
+    randomNum[8] = 0;
+
+    let code = [];
+    code = randomNum.map(function (num, index) {
+        let digit = +num;
+        let res = 0;
+        if (index % 2 !== 0) {
+            res = digit * 2;
+            if (res > 9) {
+                res -= 9;
+            }
+        } else {
+            res = digit;
+        }
+        return res;
+    });
+    console.log(code);
+
+    let sum = code.reduce(function (prev, current) {
+        return prev + current;
+    }, 0);
+    do {
+        randomNum[8] = randomNum[8] + 1;
+    } while (sum % 2 != 0);
+    console.log(sum);
+    console.log(randomNum);
+   
+    return sum % 2 === 0;
+}
+console.log(generateRandomTeudatZehut());
+
+
--- app.js
+++ app.js
@@ -1,155 +1,58 @@
-// console.log("Hello world");
+//==================CW #13=============================
+let ar = [];
+ar[10000] = 100;
+ar[1] = [1, 2, 3];
+console.log("length of array = ", ar.length);
+ar[0] = "hello";
+console.log("10000- th element = ", ar[10000]);
+console.log("0 - th element = ", ar[0]);
+console.log("1-th element = ", ar[1]);
+//-------------------------------------------------
+let str = 'Hello';
+let arStr = Array.from(str); //getting array of the string`s symbols
+console.log("String 'Hello' -> array is ", arStr);
+// professional not used "for" for seeks element at index
+for (let i = 0; i < arStr.length; i++) {
+    console.log("element at index ", i, arStr[i]);
+}
+// they used forEach:
+function printLn(element, index, array) {
+    console.log("element at index ", index, element);
+}
+arStr.forEach(printLn);
+//------------------------------------------------------
+//from array to the code ascii:
+let arCodeAscii = arStr.map(function (symbol) {
+    return symbol.charCodeAt();
+})
+console.log(arCodeAscii);
+
+let arCode_Ascii = arStr.map(function (symbol, index) {
+    return index % 2 == 0 ? symbol.charCodeAt() : symbol;
+})
+console.log(arStr, arCode_Ascii);
+
+let sumAscii = arStr.reduce(function (res, cur) {
+    return res + cur.charCodeAt();
+}, 0)
+console.log("sum of ascii ", sumAscii);
+console.log(arStr.reduce(function (res, cur) {
+    return res + cur;
+}, ""));
 
 /*
-for (var i = 0; i < 3; i++) {
-    setTimeout(function () { console.log(i) });
-} // =>3 3 3
-
-for (let i = 0; i < 3; i++) {
-    setTimeout(function () { console.log(i) });
-} // => 0 1 2
+function checkTeudatZehut(teudatStrNumber){
+    //TODO
+    control sum of for even index digit value, for odd index digit * 2
+    contril sum should be divide on 10 with no remainder
+    example 123456782 => 1 + 4 + 3 + 8 + 5 + 3 + 7 + 7 + 2 = 40 => true
+            123456783 => 1 + 4 + 3 + 8 + 5 + 3 + 7 + 7 + 3 = 41 => false
+}
+function generateRandomTeudatZehut() {
+     //TODO
+     return string of 9 symbols matching checkTeudatZehut
+     make 8 random digits from 0 to 9
+     9 - th symol should be with accordance of mathing
+     to get random digit Math.round(Math.random() * 9)
+}
 */
-//----------------------------------------------------
-/*
-for (var i = 0; i < 3; i++) {
-    console.log(i); // => 0 1 2
-}
-console.log(i); // => 3
-*/
-//-----------------------------------------------------
-// function sum(op1, op2) {
-//     let res = op1 + op2;
-//     return res;
-// }
-// console.log(sum(10, 20));
-//-----------------------------------------------------
-/*
-function sumDigits(number) {
-    //compute sum of digits for the intenger numbers
-    // if (number < 0) {
-    //     number = -number;
-    //}
-    number = Math.abs(number);
-    number = Math.trunc(number); // get rid of the fractional part
-    let sum = 0;
-    do {
-        let rem = number % 10;
-        sum += rem;
-        number = (number - rem) / 10;
-        // number = Math.floor(number / 10);
-    } while (number != 0);
-    return sum;
-}
-console.log(sumDigits(-643.876));
-*/
-//---------------------------------------------------------
-
-/***********CW #12***************/
-// let strNum1 = "123";
-// let strNum2 = "9";
-// console.log(strNum1 + strNum2); // => '1239' -string
-// console.log(strNum1 - strNum2); // => 114
-// console.log(strNum1 > strNum2); // => false
-
-// // conversions from string to number
-// let num1 = +strNum1;
-// let num2 = +strNum2;
-// console.log(num1 + num2); // => 132
-// console.log(num1 - num2); // => 114
-// console.log(num1 > num2); // => true
-
-// let strNumStr = "123.7ab";
-// let numStr = +strNumStr;
-// console.log(numStr); // => NaN - not fail. Not a Number is a number that is not a legal number. 
-// let num = parseInt(strNumStr);
-// let num3 = parseFloat(strNumStr);
-// console.log(num); // => 123;
-// console.log(num3); // => 123.7
-
-// //this's the wrong check:
-// /*
-// if(numStr == NaN) {
-//     console.log("numStr is NaN");
-// }*/
-// // this check is Ok:
-// if (isNaN(numStr)) {
-//     console.log("numStr is NaN");
-// }
-
-// let num10 = 77;
-// let strNum10 = "" + num10;
-// console.log(strNum10); // => "123" - string 10
-// let strNum16 = num10.toString(16);
-// console.log(strNum16); // => "7b" - string 16 hexadecimal
-
-// //conversion from Morse code to number
-// // symbol "." -> 0  "-" -> 1
-// function fromMorseToNumber(morseCode) {
-//     let result = 0;
-//     for (let i = 0; i < morseCode.length; i++) {
-//         let code = morseCode[i] == "." ? 0 : 1;
-//         result = result * 2 + code;
-//     }
-//     return result;
-// }
-// console.log(fromMorseToNumber("-.-....-.---...-.-")); // =>165317
-
-// function fromNumberToMorse(number) {
-//     number = Math.abs(number);
-//     let res = "";
-//     do {
-//         let digit = number % 2;
-//         let sym = digit == 0 ? "." : "-";
-//         res = sym + res;
-//         number = Math.trunc(number / 2);
-//     } while (number != 0);
-//     return res;
-// }
-// console.log(fromNumberToMorse(165317)); // => -.-....-.---...-.-
-// // --------------------------------------------------------
-//  // getting code from symbol
-// console.log('a'.charCodeAt(0));
-// // getting symbol from code
-// console.log(String.fromCharCode(97));
-
-// --------------------PATTERN------HW12-----------------
-function getSymbol(digit) {
-    let codeA = 'a'.charCodeAt();
-    if (digit > 9) {
-        digit = String.fromCharCode(codeA + digit - 10);
-    }
-    return digit;
-    // return digit < 10 ? digit : String.fromCharCode(codeA + digit - 10);
-}
-
-function fromNumberToString(number, base) {
-    number = Math.abs(number);
-    let res = "";
-    do {
-        let digit = number % base;
-        let sym = getSymbol(digit);
-        res = sym + res;
-        number = Math.trunc(number / base);
-    } while (number != 0);
-    return res;
-}
-console.log(fromNumberToString(900550, 36));
-
-function getDigit(symbol) {
-    let codeA = 'a'.charCodeAt();
-    let res = symbol < '9' ? + symbol : symbol.charCodeAt() - codeA + 10;
-    return res;
-}
-
-function fromStringToNumber(string, base) {
-    string = string.toLowerCase();
-    let result = 0;
-    for (let i = 0; i < string.length; i++) {
-        let digit = getDigit(string[i]);
-        result = result * base + digit;
-    }
-    return result;
-}
-console.log(fromStringToNumber("java", 36));
-
-
