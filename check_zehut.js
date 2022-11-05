b490007fe32e07efe80fd54b6985c3857ddc9848
EvgenyPankov <volvovnl123@gmail.com>
2022-11-05 18:12:03 +0200

    correction check_zehut

--- check_zehut.js
+++ check_zehut.js
@@ -35,9 +35,9 @@ function generateRandomTeudatZehut() {
     let randomArr = [];
     for (let i = 0; i < 8; i++) {
         randomArr.push(Math.round(Math.random() * 9));
-    }    
+    }
     randomArr[8] = 0;
-    
+
     let code = [];
     code = randomArr.map(function (num, index) {
         let digit = +num;
@@ -60,21 +60,23 @@ function generateRandomTeudatZehut() {
     if (sum % 10 !== 0) {
         let randomNum = Math.floor(sum / 10) * 10;
         if (randomNum !== sum) {
+            console.log(randomNum);
             randomArr[8] = randomNum + 10 - sum;
         }
     }
 
     return sum % 10 !== 0;
 }
-console.log(generateRandomTeudatZehut());
+console.log("result = ", generateRandomTeudatZehut());
 
 //=============Vebinar cod====================
-const nineDigits = '012345678';
+
+const nineDigits = "012345678";
 const minDigit = 0;
 const maxDigit = 9;
 const char0 = '0'.charCodeAt();
-function checkTeudatZehut(tzStr) {
-    if(tzStr.length != nineDigits.length || isNaN(+tzStr)) {
+function checkTeudatZehut1(tzStr) {
+    if (tzStr.length != nineDigits.length || isNaN(+tzStr)) {
         console.log("TZ=", tzStr, 'valid=', false);
         return false;
     }
@@ -93,15 +95,15 @@ function getControlSum(tzStr) {
         return sum+cur;
     }, 0);
     */
-    return Array.from(tzStr).map(function(symbol, ind) {
+    return Array.from(tzStr).map(function (symbol, ind) {
         let value = symbol.charCodeAt() - char0;
-        return ind % 2 == 0 ? getEvenValue(value) : getOddValue(value*2);
-    }).reduce(function(sum, cur) {
-        return sum+cur;
+        return ind % 2 == 0 ? getEvenValue(value) : getOddValue(value * 2);
+    }).reduce(function (sum, cur) {
+        return sum + cur;
     }, 0);
 }
 function getOddValue(number) {
-    return number<10 ? number : number%10 + Math.trunc(number/10);
+    return number < 10 ? number : number % 10 + Math.trunc(number / 10);
 }
 function getEvenValue(number) {
     return number;
@@ -110,41 +112,41 @@ function getEvenValue(number) {
 function generateTeudatZehut() {
     let array = getGeneratedArray();
     array[8] = updateCtrlDigit(array);
-    if(!checkTeudatZehut(integerArray2String(array))) {
+    if (!checkTeudatZehut1(integerArray2String(array))) {
         console.log('Generation failed');
     }
 }
 function getGeneratedArray() {
-    return Array.from(nineDigits).map(function(symbol, ind) {
-        let value = (ind==8) ? 0 : getRandomIntegerValue(minDigit, maxDigit+1);
-        return ind % 2 == 0 ? getEvenValue(value) : getOddValue(value*2);
+    return Array.from(nineDigits).map(function (symbol, ind) {
+        let value = (ind == 8) ? 0 : getRandomIntegerValue(minDigit, maxDigit + 1);
+        return ind % 2 == 0 ? getEvenValue(value) : getOddValue(value * 2);
     });
 }
 function integerArray2String(array) {
-    return array.reduce(function(str, cur) {
-        return str + String.fromCharCode(cur+char0);
+    return array.reduce(function (str, cur) {
+        return str + String.fromCharCode(cur + char0);
     }, "");
 }
 function updateCtrlDigit(array) {
     let sum = getControlSum(integerArray2String(array));
-    let roundedSum = Math.floor(sum/10)*10;
-    if(roundedSum === sum) {
+    let roundedSum = Math.floor(sum / 10) * 10;
+    if (roundedSum === sum) {
         return 0;
-    } 
-    return roundedSum+10-sum;
+    }
+    return roundedSum + 10 - sum;
 }
 function getRandomIntegerValue(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     // The maximum is exclusive and the minimum is inclusive
-    return Math.floor(Math.random() * (max - min) + min); 
+    return Math.floor(Math.random() * (max - min) + min);
 }
 // Tests
-checkTeudatZehut('311769103');
-checkTeudatZehut('311769129');
-checkTeudatZehut('311759129');
-checkTeudatZehut('336097183');
-checkTeudatZehut('33609183');
-checkTeudatZehut('33609as83');
-checkTeudatZehut('012345674');
+checkTeudatZehut1('311769103');
+checkTeudatZehut1('311769129');
+checkTeudatZehut1('311759129');
+checkTeudatZehut1('336097183');
+checkTeudatZehut1('33609183');
+checkTeudatZehut1('33609as83');
+checkTeudatZehut1('012345674');
 generateTeudatZehut();
