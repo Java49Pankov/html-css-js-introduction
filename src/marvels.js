54429c4cfa5701d64fc6a3a9d9609576fade6689
EvgenyPankov <volvovnl123@gmail.com>
2022-11-19 12:01:30 +0200

    random index, search by letter

--- src/marvels.js
+++ src/marvels.js
@@ -1,52 +1,60 @@
 const words = ["syntax", "loop", "array"];
 const questions = ["Is the set of rules, how JavaScript programs are constructed", "Can execute a block of code a number of times.",
     "Is a special variable, which can hold more than one value"];
-let INDEX;
-let WORDFIELD;
-const N_LETTERS = '';
+let index = 0;
+let wordField;
+let countLetter = 0;
+let wordFieldAr;
+let letterElements;
+let prevInd = -1;
 const sectionElement = document.querySelector(".word-guess");
 const divQuestion = document.querySelector(".question-guess");
-let letterElements;
 const gameOverElement = document.querySelector(".game-over-message");
 const playAgainElement = document.getElementById("play-again");
-
-playAgainElement.addEventListener('click', startGame);
 
 function startGame() {
     gameOverElement.innerHTML = '';
     sectionElement.innerHTML = getDivsElements();
     letterElements = document.querySelectorAll(".letter-guess");
-    console.log("startGame");
 }
 
 function getDivsElements() {
-    INDEX = Math.floor(Math.random() * words.length);
-    WORDFIELD = words[INDEX];
-    divQuestion.innerHTML = questions[INDEX];
-    let wordFieldAr = Array.from(WORDFIELD);
+    index = getIndex();
+    wordField = words[index];
+    divQuestion.innerHTML = questions[index];
+    wordFieldAr = Array.from(wordField);
     let res = wordFieldAr.map(letter => `<div class ="letter-guess">${letter}</div>`);
     return res.join('');
+}
+
+function getIndex() {
+    index = Math.floor(Math.random() * words.length);
+    while (prevInd == index) {
+        index = Math.floor(Math.random() * words.length);
+    }
+    prevInd = index;
+    console.log(prevInd);
+    return index;
 }
 
 function onChange(event) {
     const wordGuess = event.target.value;
     event.target.value = '';
     const wordAr = Array.from(wordGuess);
-    const colors = wordAr.map((l, i) => {
-        let res = "";
-        if (l == WORDFIELD[i]) {
-            res = "white"
-        }
-        return res;
-    })
-    colors.forEach((c, i) => {
-        console.log(colors, letterElements);
-        if (letterElements[i].style.background != 'white') {
-            letterElements[i].style.background = c
+    const colors = wordFieldAr.map((letter) => {
+        return wordAr.includes(letter) ? "white" : "black";
+    });
+    colors.forEach((color, index) => {
+        if (color == "white") {
+            if (letterElements[index].style.background != 'white') {
+                countLetter++;
+                letterElements[index].style.background = color
+            }
         }
     });
-    const res = wordGuess == WORDFIELD;
-    endGame(res);
+    if (countLetter == wordFieldAr.length) {
+        endGame(true);
+    }
 }
 
 function endGame(value) {
@@ -55,6 +63,7 @@ function endGame(value) {
         gameOverElement.style.color = "green"
     }
     playAgainElement.style.display = 'block';
+    countLetter = 0;
 }
 
 startGame();
