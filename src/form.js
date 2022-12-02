1a13de2707091f7754b20efddc968acf5f6729d4
EvgenyPankov <volvovnl123@gmail.com>
2022-12-02 14:28:06 +0200

    final solution HW21

--- src/form.js
+++ src/form.js
@@ -1,6 +1,7 @@
 const ERROR = "error"
 const curYear = new Date().getFullYear();
-console.log(curYear);
+console.log(curYear)
+
 const inputElements = document.querySelectorAll(".form-class [name]");
 const detailError = document.querySelector(".detail-error")
 
@@ -16,18 +17,21 @@ function onSubmit(event) {
 
 function onChange(event) {
     if (event.target.name == "salary" && validateSalary(event.target.value)) {
-        event.target.value = showError(event.target);
+        event.target.value = showError(event.target, `a valid salary`);
     } else if (event.target.name == "email" && validateEmail(event.target.value)) {
-        event.target.value = showError(event.target);
+        event.target.value = showError(event.target, `a valid email`);
     } else if (event.target.name = "employee_name" && validateName(event.target.value)) {
-        event.target.value = showError(event.target);
-    } else if (event.target.name == "birthDate") {
-        const arr = (event.target.value).split("-");
-        if (arr[0] < 1950 || arr[0] > curYear) {
-            event.target.value = showError(event.target);
-        }
+        event.target.value = showError(event.target, `a valid name`);
+    } else if (event.target.name = "birthDate" && validateBirthDate(event.target.value)) {
+        event.target.value = showError(event.target, `a valid birthYear`);
     }
 }
+
+function validateBirthDate(birthDate) {
+    const arr = birthDate.split("-");
+    return +arr[0] < 1950 || +arr[0] > curYear;
+}
+
 function validateSalary(salary) {
     return +salary < 1000 || +salary > 40000;
 }
@@ -40,9 +44,9 @@ function validateName(name) {
     return name.length < 2 || name.length == 0 || name.length > 32;
 }
 
-function showError(element) {
+function showError(element, text) {
     element.classList.add(ERROR);
-    detailError.textContent = "wrong input";
+    detailError.textContent = "ERROR: enter " + text;
     setTimeout(function () {
         element.classList.remove(ERROR);
         detailError.textContent = "";
