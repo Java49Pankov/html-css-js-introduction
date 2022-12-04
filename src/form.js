fb676b9e29e7d88df0f3b0ef8ae5548f5c296724
EvgenyPankov <volvovnl123@gmail.com>
2022-12-04 17:47:19 +0200

    forms , teachers solution HW #21

--- src/form.js
+++ src/form.js
@@ -1,55 +1,62 @@
-const ERROR = "error"
-const curYear = new Date().getFullYear();
-console.log(curYear)
-
 const inputElements = document.querySelectorAll(".form-class [name]");
-const detailError = document.querySelector(".detail-error")
+const MIN_SALARY = 1000;
+const MAX_SALARY = 40000;
+const MIN_YEAR = 1950;
+const maxYear = getMaxYear();
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const ERROR_CLASS = "error";
+const dateErrorElement = document.getElementById("date_error");
+const salaryErrorElement = document.getElementById("salary_error");
 
 function onSubmit(event) {
     event.preventDefault();
+    console.log("submitted");
     const employee = Array.from(inputElements).reduce(
         (res, cur) => {
             res[cur.name] = cur.value;
             return res;
         }, {}
     )
+    console.log(employee)
+    
 }
-
 function onChange(event) {
-    if (event.target.name == "salary" && validateSalary(event.target.value)) {
-        event.target.value = showError(event.target, `a valid salary`);
-    } else if (event.target.name == "email" && validateEmail(event.target.value)) {
-        event.target.value = showError(event.target, `a valid email`);
-    } else if (event.target.name = "employee_name" && validateName(event.target.value)) {
-        event.target.value = showError(event.target, `a valid name`);
-    } else if (event.target.name = "birthDate" && validateBirthDate(event.target.value)) {
-        event.target.value = showError(event.target, `a valid birthYear`);
+
+    if (event.target.name == "salary") {
+        validateSalary(event.target)
+    } else if (event.target.name == "birthDate") {
+        validateBirthdate(event.target);
     }
 }
+function validateSalary(element) {
+    const value = +element.value;
+    if (value < MIN_SALARY || value > MAX_SALARY) {
+        const message = value < MIN_SALARY ? `salary must be ${MIN_SALARY} or greater`
+            : `salary must be ${MAX_SALARY} or less`;
+        showErrorMessage(element, message, salaryErrorElement);
+    }
 
-function validateBirthDate(birthDate) {
-    const arr = birthDate.split("-");
-    return +arr[0] < 1950 || +arr[0] > curYear;
+}
+function validateBirthdate(element) {
+    const value = +element.value.slice(0, 4);
+    if (value < MIN_YEAR || value > maxYear) {
+        const message = value < MIN_YEAR ? `year must be ${MIN_YEAR} or greater`:
+             `year must be ${maxYear} or less`;
+        showErrorMessage(element, message, dateErrorElement) ;    
+
+    }
+
+}
+function showErrorMessage(element, message, errorElement) {
+    element.classList.add(ERROR_CLASS);
+    errorElement.innerHTML = message;
+    setTimeout(() => {
+        element.classList.remove(ERROR_CLASS);
+        element.value = ''; 
+        errorElement.innerHTML = '';
+    }, TIME_OUT_ERROR_MESSAGE);
 }
 
-function validateSalary(salary) {
-    return +salary < 1000 || +salary > 40000;
-}
-
-function validateEmail(email) {
-    return !email.split("").includes("@");
-}
-
-function validateName(name) {
-    return name.length < 2 || name.length == 0 || name.length > 32;
-}
-
-function showError(element, text) {
-    element.classList.add(ERROR);
-    detailError.textContent = "ERROR: enter " + text;
-    setTimeout(function () {
-        element.classList.remove(ERROR);
-        detailError.textContent = "";
-    }, 5000)
-    return "";
+function getMaxYear() {
+    return new Date().getFullYear();
 }
