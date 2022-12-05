aaa5bc3227141fc407beb545d00ff3ec3d4a2308
EvgenyPankov <volvovnl123@gmail.com>
2022-12-05 18:43:56 +0200

    3 buttons

--- src/form.js
+++ src/form.js
@@ -1,9 +1,13 @@
 const ERROR = "error"
 const curYear = new Date().getFullYear();
-console.log(curYear)
+const MIN_SALARY = 1000;
+const MAX_SALARY = 40000;
+const MIN_YEAR = 1950;
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const company = new Company();
 
 const inputElements = document.querySelectorAll(".form-class [name]");
-const detailError = document.querySelector(".detail-error")
+const detailError = document.querySelector(".detail-error");
 
 function onSubmit(event) {
     event.preventDefault();
@@ -13,6 +17,9 @@ function onSubmit(event) {
             return res;
         }, {}
     )
+    company.hireEmployee(employee);
+    console.log(employee);
+    console.log(company)
 }
 
 function onChange(event) {
@@ -20,20 +27,20 @@ function onChange(event) {
         event.target.value = showError(event.target, `a valid salary`);
     } else if (event.target.name == "email" && validateEmail(event.target.value)) {
         event.target.value = showError(event.target, `a valid email`);
-    } else if (event.target.name = "employee_name" && validateName(event.target.value)) {
+    } else if (event.target.name == "employee_name" && validateName(event.target.value)) {
         event.target.value = showError(event.target, `a valid name`);
-    } else if (event.target.name = "birthDate" && validateBirthDate(event.target.value)) {
+    } else if (event.target.name == "birthDate" && validateBirthDate(event.target.value)) {
         event.target.value = showError(event.target, `a valid birthYear`);
     }
 }
 
 function validateBirthDate(birthDate) {
     const arr = birthDate.split("-");
-    return +arr[0] < 1950 || +arr[0] > curYear;
+    return +arr[0] < MIN_YEAR || +arr[0] > curYear;
 }
 
 function validateSalary(salary) {
-    return +salary < 1000 || +salary > 40000;
+    return +salary < MIN_SALARY || +salary > MAX_SALARY;
 }
 
 function validateEmail(email) {
@@ -41,7 +48,7 @@ function validateEmail(email) {
 }
 
 function validateName(name) {
-    return name.length < 2 || name.length == 0 || name.length > 32;
+    return name.length < 2 || name.length == 0 || name.length > 16;
 }
 
 function showError(element, text) {
@@ -50,6 +57,21 @@ function showError(element, text) {
     setTimeout(function () {
         element.classList.remove(ERROR);
         detailError.textContent = "";
-    }, 5000)
+    }, TIME_OUT_ERROR_MESSAGE)
     return "";
+}
+
+function Company() {
+    this.employees = [];
+}
+Company.prototype.hireEmployee = function (employee) {
+    this.employees.push(employee);
+}
+
+Company.prototype.getAllEmployees = function () {
+    return this.employees;
+}
+
+Company.prototype.getAllEmployeesBySalary = function (salaryFrom, salaryTo) {
+    //TODO
 }
